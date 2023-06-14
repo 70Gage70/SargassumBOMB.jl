dcd2bbff99be6b160901fc9539d21686f6f74372
Gage <gage.bonner@gmail.com>
2023-06-13 20:54:27 -0400

    large system?

--- src/main3.jl
+++ src/main3.jl
@@ -18,99 +18,104 @@ ref = EquirectangularReference(lon0 = -75.0, lat0 = 10.0)
 @info "Generating model."
 
 x0, y0 = sph2xy(-64, 14, ref) 
-x_range = range(start = x0 - 1, length = 5, stop = x0 + 1)
-y_range = range(start = y0 - 1, length = 5, stop = y0 + 1)
+x_range = range(start = x0 - 5, length = 25, stop = x0 + 5)
+y_range = range(start = y0 - 5, length = 25, stop = y0 + 5)
 clump_parameters = BOMParameters(ref)
 spring_parameters = SpringParameters(k -> 20, 0.4)
 
 @named RRaft = RectangularRaft(x_range, y_range, clump_parameters, spring_parameters)
 RRaft = structural_simplify(RRaft)
 
-t_range = (0.0, 150.0)
-# t_range = (0.0, 2.0)
+# t_range = (0.0, 150.0)
+t_range = (0.0, 1.0)
+
+@info "Generating problem."
 
 prob = ODEProblem(
     RRaft, 
     [],
     t_range, 
-    []
+    [],
+    jac = true,
+    sparse = true
 )
 
-@info "Solving model."
 
-sol = solve(prob)
+# @info "Solving model."
 
-@variables COM(t)[1:2]
-com = [sol[COM[1]] ;; sol[COM[2]]]
-traj = xy2sph(com, ref)
-lon_traj, lat_traj = (traj[:, 1], traj[:, 2]) 
-times = sol.t
+# sol = solve(prob)
 
-@info "Plotting results."
+# @variables COM(t)[1:2]
+# com = [sol[COM[1]] ;; sol[COM[2]]]
+# traj = xy2sph(com, ref)
+# lon_traj, lat_traj = (traj[:, 1], traj[:, 2]) 
+# times = sol.t
 
-function x_labeler(x::Real)
-    if x > 0.0
-        return L"%$(x)^\circ\,\textrm{E}"
-    elseif x == 0.0
-        return "0^\circ"
-    else if x < 0.0 
-        return L"%$(x)^\circ\,\textrm{W}"
-    end
-end
+# @info "Plotting results."
 
-function y_labeler(y::Real)
-    if y > 0.0
-        return "$(y)^\circ\,\textrm{N}"
-    elseif y == 0.0
-        return "0^\circ"
-    else if y < 0.0 
-        return "$(y)^\circ\,\textrm{S}"
-    end
-end
+# function x_labeler(x::Real)
+#     if x > 0.0
+#         return L"%$(x)^\circ\,\textrm{E}"
+#     elseif x == 0.0
+#         return "0^\circ"
+#     else if x < 0.0 
+#         return L"%$(x)^\circ\,\textrm{W}"
+#     end
+# end
 
-fig = Figure(
-    resolution = (1920, 1080), 
-    fontsize = 50,
-    figure_padding = (5, 50, 5, 5));
+# function y_labeler(y::Real)
+#     if y > 0.0
+#         return "$(y)^\circ\,\textrm{N}"
+#     elseif y == 0.0
+#         return "0^\circ"
+#     else if y < 0.0 
+#         return "$(y)^\circ\,\textrm{S}"
+#     end
+# end
 
-ax = Axis(
-    fig[1, 1],
-    limits = (-100, -50, 5, 35), 
-    title = L"\textrm{COM of a } 5 \times 5 \textrm{ raft}",
-    xticklabelsize = 40,
-    yticklabelsize = 40,
-    xtickformat = values -> [
-        if value > 0 
-            L"%$(abs(value))^\circ \, \mathrm{E}" 
-        elseif value == 0 
-            L"0^\circ"
-        elseif value < 0
-            L"%$(abs(value))^\circ \, \mathrm{W}" 
-        end
-    for value in values],
-    ytickformat = values -> [
-        if value > 0 
-            L"%$(abs(value))^\circ \, \mathrm{N}" 
-        elseif value == 0 
-            L"0^\circ"
-        elseif value < 0
-            L"%$(abs(value))^\circ \, \mathrm{S}" 
-        end
-    for value in values]
-);
+# fig = Figure(
+#     resolution = (1920, 1080), 
+#     fontsize = 50,
+#     figure_padding = (5, 50, 5, 5));
 
-lon, lat, data = GeoDatasets.landseamask(; resolution = 'i', grid = 1.25);
-contour!(ax, lon, lat, data, levels = [0.5], color = :black);
+# ax = Axis(
+#     fig[1, 1],
+#     limits = (-100, -50, 5, 35), 
+#     title = L"\textrm{COM of a } 5 \times 5 \textrm{ raft}",
+#     xticklabelsize = 40,
+#     yticklabelsize = 40,
+#     xtickformat = values -> [
+#         if value > 0 
+#             L"%$(abs(value))^\circ \, \mathrm{E}" 
+#         elseif value == 0 
+#             L"0^\circ"
+#         elseif value < 0
+#             L"%$(abs(value))^\circ \, \mathrm{W}" 
+#         end
+#     for value in values],
+#     ytickformat = values -> [
+#         if value > 0 
+#             L"%$(abs(value))^\circ \, \mathrm{N}" 
+#         elseif value == 0 
+#             L"0^\circ"
+#         elseif value < 0
+#             L"%$(abs(value))^\circ \, \mathrm{S}" 
+#         end
+#     for value in values]
+# );
 
-ln = lines!(ax, lon_traj, lat_traj; color = times, linewidth = 4);
-Colorbar(
-    fig[1,2], 
-    ln, 
-    label = L"\textrm{Days}",
-    ticklabelsize = 40, 
-    ticks = [0, 50, 100, 150], 
-    tickformat = values -> [L"%$(value)" for value in values],
-    height = Relative(2/4)
-);
+# lon, lat, data = GeoDatasets.landseamask(; resolution = 'i', grid = 1.25);
+# contour!(ax, lon, lat, data, levels = [0.5], color = :black);
 
-fig
+# ln = lines!(ax, lon_traj, lat_traj; color = times, linewidth = 4);
+# Colorbar(
+#     fig[1,2], 
+#     ln, 
+#     label = L"\textrm{Days}",
+#     ticklabelsize = 40, 
+#     ticks = [0, 50, 100, 150], 
+#     tickformat = values -> [L"%$(value)" for value in values],
+#     height = Relative(2/4)
+# );
+
+# fig

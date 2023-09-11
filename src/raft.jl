6e242db020d9663a1c3606b2cb24fd20e89954c3
Gage <gage.bonner@gmail.com>
2023-09-10 21:39:56 -0400

    have hist

--- src/raft.jl
+++ src/raft.jl
@@ -11,8 +11,8 @@ include(joinpath(@__DIR__, "../../CustomMakie.jl/src/statistic-methods.jl"))
 @info "Generating model."
 
 # x0, y0 = -90, 23 # GoM
-# x0, y0 = -64, 14
-x0, y0 = -55, 10
+x0, y0 = -64, 14
+# x0, y0 = -55, 10
 
 # x_range, y_range = sph2xy(range(x0 - 1, x0 + 1, step = 0.5), range(y0 - 1, y0 + 1, step = 0.5), ref_itp)
 x_range, y_range = sph2xy(range(x0 - 2, x0 + 2, step = 0.3), range(y0 - 2, y0 + 2, step = 0.3), ref_itp)
@@ -28,7 +28,8 @@ function spring_k(x::Real; A::Real = 5.0, k10::Real = 2*step(x_range))
 end
 
 # gd_model = ImmortalModel()
-gd_model = BrooksModel(params = BrooksModelParameters(temp_itp, no3_itp, clumps_limits = (0, 350)), verbose = true)
+# gd_model = BrooksModel(params = BrooksModelParameters(temp_itp, no3_itp, clumps_limits = (0, 350)), verbose = true)
+gd_model = BrooksModel(verbose = true)
 rp = RaftParameters(x_range, y_range, cp, spring_k, first(tspan), "full", gd_model)
 
 prob_raft = ODEProblem(Raft!, rp.ics, tspan, rp)
@@ -42,7 +43,6 @@ land = Land(verbose = true)
     callback = CallbackSet(cb_loc2label(), callback(land), callback(gd_model))
 );
 
-rtr = RaftTrajectory(sol_raft, rp, ref_itp)
 
 @info "Generating reference clump."
 
@@ -60,23 +60,21 @@ ctr = RaftTrajectory(sol_clump, rp_1c, ref_itp)
 
 @info "Plotting results."
 
-# limits = (-100, -50, 5, 35)
-limits = (-70, -50, 5, 25)
-
-### Trajectory COM
+limits = (-100, -50, 5, 35)
+# limits = (-70, -50, 5, 25)
 
 fig_COM = default_fig()
 ax = geo_axis(fig_COM[1, 1], limits = limits, title = L"\mathrm{Raft COM}")
 
-# raft
-trajectory!(ax, rtr)
+### raft
+# trajectory!(ax, rtr)
 
-# COM
-trajectory!(ax, rtr.com, 
-    opts = (linestyle = :dot, color = rtr.com.t, colormap = :heat, linewidth = 5)
-) 
+### COM
+# trajectory!(ax, rtr.com, 
+#     opts = (linestyle = :dot, color = rtr.com.t, colormap = :heat, linewidth = 5)
+# ) 
 
-# COM 5
+### COM 5
 # rtr_5 = RaftTrajectory(sol_raft, rp, ref_itp, dt = 5.0)
 
 # trajectory!(ax, rtr_5.com, 
@@ -85,14 +83,26 @@ trajectory!(ax, rtr.com,
 
 # scatter!(ax, rtr_5.com.xy[:,1], rtr_5.com.xy[:,2])
 
-# clump
-trajectory!(ax, ctr, 
-    opts = (color = ctr.t, colormap = :heat, linewidth = 2)
-) 
+### clump
+# trajectory!(ax, ctr, 
+#     opts = (color = ctr.t, colormap = :heat, linewidth = 2)
+# ) 
+
+### hist
+rtr_dt = RaftTrajectory(sol_raft, rp, ref_itp, dt = 1.0)
+tr_hist = trajectory_hist!(ax, rtr_dt, opts = (cellsize = 0.5,))
+
 
 land!(ax)
 
-tticks = collect(range(start = minimum(rtr.t), stop = maximum(rtr.t), length = 5))
-data_legend!(fig_COM[1,2], L"\mathrm{Days}", ticks = tticks)
+### days legend
+# tticks = collect(range(start = minimum(rtr.t), stop = maximum(rtr.t), length = 5))
+# data_legend!(fig_COM[1,2], L"\mathrm{Days}", ticks = tticks)
+
+### counts legend
+min_cts, max_cts = getindex(tr_hist.colorrange)
+tticks = collect(range(start = min_cts, stop = max_cts, length = 5))
+data_legend!(fig_COM[1,2], L"\mathrm{Counts}", ticks = tticks)
 
 fig_COM
+

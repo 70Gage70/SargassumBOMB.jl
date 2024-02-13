f03169fba5ba1f654be9a4d1487dcccd3108954d
Gage <gage.bonner@gmail.com>
2024-02-12 22:27:32 -0500

    fix landseamask

--- interpolants/land/itp-construct.jl
+++ interpolants/land/itp-construct.jl
@@ -5,7 +5,7 @@ function construct_land_itp()
     @info "Constructing land interpolant."
 
     # 0 is ocean, 1 is land and 2 is lake
-    lon, lat, land = GeoDatasets.landseamask(size = 'c', grid = 5)
+    lon, lat, land = GeoDatasets.landseamask(resolution = 'c', grid = 5)
     land[land .== 2] .= 1 # lake is not ocean, so it's land
 
     itp = GriddedField([:lon, :lat], Dict(:lon => lon, :lat => lat), Dict(:land => land), nothing, nothing, nothing, EQR_DEFAULT)
